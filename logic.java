1a16b7f809f045694fceb774eb4da1a751c99327
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-05 14:58:02 +0530

    Add files via upload

---
+++ logic.java
@@ -0,0 +1,18 @@
+ import java.util.*;
+ public class logic {
+    public static void main(String[] args){
+    int n=16;
+    for(int i=0;i*i<=n;i++)
+    {
+        if(n%i==0)
+        c+=1;
+        {
+          if(i*i!=n)
+          {
+            c+=1;
+          }
+        } 
+        System.out.println(c);     
+ }
+}
+}
